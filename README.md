de50fe2e7fa9a73a7cbac058713832c00fe9219c
幻彩冰袭 <symphoneice@SymphoneIcedeMacBook-Pro.local>
2025-04-25 13:55:46 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Install nix
+https://nix.dev/install-nix
+## Macos
+`curl -L https://nixos.org/nix/install | sh`
+
+# clone
+`git clone https://github.com/SymphonyIceAttack/nix-darwin/new/main`
+# build
+`darwin-rebuild build --flake .#SymphoneIcedeMacBook-Pro
+`
---
+++ flake.nix
@@ -0,0 +1,201 @@
+{
+  description = "SymphoneIce nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    mac-app-util.url = "github:hraban/mac-app-util";
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    inputs@{
+      self,
+      nix-darwin,
+      nixpkgs,
+      mac-app-util,
+      nix-homebrew,
+      home-manager,
+    }:
+    let
+      nixpkgs.config.allowUnfree = true;
+
+      configuration =
+        { pkgs, config, ... }:
+        {
+          # List packages installed in system profile. To search by name, run:
+          # $ nix-env -qaP | grep wget
+          environment.systemPackages = [
+            pkgs.mkalias
+            pkgs.cargo
+            pkgs.go
+            pkgs.lua
+            pkgs.git
+            pkgs.pyright
+            pkgs.nixfmt-rfc-style
+            pkgs.fzf
+            pkgs.ripgrep
+            pkgs.lazygit
+            pkgs.wget
+            pkgs.gopls
+            pkgs.devenv
+            pkgs.direnv
+          ];
+          environment.shellAliases = {
+            gitClean = "git branch --merged | egrep -v ${"(^\*|master)"} | xargs git branch -d";
+          };
+          fonts.packages = [
+            pkgs.nerd-fonts.jetbrains-mono
+            pkgs.nerd-fonts.hack
+          ];
+
+          homebrew = {
+            enable = true;
+            brews = [
+              "mas"
+              "python"
+              "neovim"
+            ];
+            casks = [
+              {
+                name = "docker";
+                greedy = true;
+              }
+              {
+                name = "input-source-pro";
+                greedy = true;
+              }
+              {
+                name = "input-source-pro";
+
+              }
+              {
+                name = "telegram";
+                greedy = true;
+              }
+              {
+                name = "warp";
+                greedy = true;
+              }
+              {
+                name = "discord";
+                greedy = true;
+              }
+              {
+                name = "spotify";
+                greedy = true;
+              }
+              {
+                name = "wechat";
+                greedy = true;
+              }
+              {
+                name = "qq";
+                greedy = true;
+              }
+              {
+                name = "arc";
+                greedy = true;
+              }
+            ];
+            masApps = {
+
+            };
+            onActivation = {
+              cleanup = "zap";
+              autoUpdate = true;
+              upgrade = true;
+            };
+            taps = [
+              #  {
+              #   name="user/repo";
+              #   clone_target="githuburl";
+              #   force_auto_update=true;
+              #  }
+            ];
+          };
+
+          system.defaults = {
+            dock.autohide = true;
+            dock.persistent-apps = [
+              "/System/Applications/Launchpad.app"
+              "/System/Applications/System Settings.app"
+              "/System/Applications/Messages.app"
+              "/System/Applications/Mail.app"
+              "/System/Applications/App Store.app"
+              "/Applications/Telegram.app"
+              "/Applications/Surge.app"
+              "/Applications/Steam.app"
+              "/Applications/Spotify.app"
+              "/Applications/Discord.app"
+              "/Applications/WeChat.app"
+              "/Applications/QQ.app"
+              "/Applications/Arc.app"
+              "/Applications/Warp.app"
+            ];
+            dock.mru-spaces = false;
+            loginwindow.GuestEnabled = false;
+            NSGlobalDomain.AppleICUForce24HourTime = true;
+            NSGlobalDomain.AppleInterfaceStyle = "Dark";
+            NSGlobalDomain.KeyRepeat = 2;
+          };
+          # Necessary for using flakes on this system.
+          nix.settings.experimental-features = "nix-command flakes";
+          nix.settings.trusted-users = [
+            "root"
+            "symphoneice"
+          ];
+          # Enable alternative shell support in nix-darwin.
+          # programs.fish.enable = true;
+          # Set Git commit hash for darwin-version.
+          system.configurationRevision = self.rev or self.dirtyRev or null;
+
+          # Used for backwards compatibility, please read the changelog before changing.
+          # $ darwin-rebuild changelog
+          system.stateVersion = 6;
+
+          # The platform the configuration will be used on.
+          nixpkgs.hostPlatform = "aarch64-darwin";
+        };
+    in
+    {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#SymphoneIcedeMacBook-Pro
+      darwinConfigurations."SymphoneIcedeMacBook-Pro" = nix-darwin.lib.darwinSystem {
+        modules = [
+          configuration
+          # This is a Nix module for Macintosh computers (“darwin”)
+          # which fixes a few common problems encountered by users of
+          # Nix on Macs:
+          mac-app-util.darwinModules.default
+          nix-homebrew.darwinModules.nix-homebrew
+          {
+            nix-homebrew = {
+              # Install Homebrew under the default prefix
+              enable = true;
+
+              # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+              enableRosetta = true;
+
+              # User owning the Homebrew prefix
+              user = "symphoneIce";
+
+              # Automatically migrate existing Homebrew installations
+              autoMigrate = true;
+              # Optional: Declarative tap management
+              taps = {
+              };
+            };
+          }
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+          }
+        ];
+      };
+    };
+}
